e35ce185d883b9720b5e317906418825db0b5e62
Carlton W <carltonsaddress@hotnail.com>
2024-09-27 22:17:54 +0000

    added workers for gunicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,4 @@
 python3 manage.py migrate --no-input
 python3 manage.py collectstatic --no-input
 
-gunicorn locallibrary.wsgi:application --bind 0.0.0.0:8080
+gunicorn locallibrary.wsgi:application --bind 0.0.0.0:8080 --workers 3
